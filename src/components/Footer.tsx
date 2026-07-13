cb04979cd73964b7cc0c784f0325b100501c41d7
kangcommit <ahmad.codespace@gmail.com>
2026-07-13 22:08:26 +0700

    style: simplify Footer component structure and update copyright symbol

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
 	return (
-		<footer className="bg-[#252525] py-6">
-			<p className="text-center font-medium text-[#AAAAAA] text-sm">
-				Ⓒ 2026 #VANLIFE
-			</p>
+		<footer className="bg-[#252525] py-6 text-center font-medium text-[#AAAAAA] text-sm">
+			&#169; 2026 #VANLIFE
 		</footer>
 	);
 }
