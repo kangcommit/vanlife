7000c103d50a797f09f18c5bf7a6e90c4f37f038
kangcommit <ahmad.codespace@gmail.com>
2026-07-16 00:08:34 +0700

    fix(vans): add loading state and spinner during data fetch

--- src/modules/vans/pages/Vans.tsx
+++ src/modules/vans/pages/Vans.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router";
+import LoadingSpinner from "../../../components/LoadingSpinner";
 import type { Van, VansResponse } from "../../../utils/types";
 import VanCard from "../components/VanCard";
 import VanTypeFilter from "../components/VanTypeFilter";
@@ -7,13 +8,17 @@ import VanTypeFilter from "../components/VanTypeFilter";
 export default function Vans() {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [vans, setVans] = React.useState<Van[]>([]);
+	const [loading, setLoading] = React.useState(true);
 
 	const typeFilter = searchParams.get("type");
 
 	React.useEffect(() => {
+		setLoading(true);
+
 		fetch("/api/vans")
 			.then((res) => res.json())
-			.then((data: VansResponse) => setVans(data.vans));
+			.then((data: VansResponse) => setVans(data.vans))
+			.finally(() => setLoading(false));
 	}, []);
 
 	const displayedVans = typeFilter
@@ -30,31 +35,37 @@ export default function Vans() {
 		setSearchParams({});
 	}
 
-	return (
-		<section className="mx-auto mt-13.5 mb-21 max-w-7xl px-6">
-			<h1 className="mb-6 font-bold text-3xl">Explore our van options</h1>
-			<div className="mb-10 flex flex-wrap items-center justify-between gap-4">
-				<VanTypeFilter
-					types={uniqueTypes}
-					selectedType={typeFilter}
-					onSelect={handleSelectFilter}
-				/>
+	if (loading) {
+		return <LoadingSpinner />;
+	}
 
-				{typeFilter && (
-					<button
-						type="button"
-						onClick={handleClearFilter}
-						className="cursor-pointer font-medium text-slate hover:underline"
-					>
-						Clear filters
-					</button>
-				)}
-			</div>
-			<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-				{displayedVans.map((van) => (
-					<VanCard key={van.id} van={van} typeFilter={typeFilter} />
-				))}
-			</div>
-		</section>
-	);
+	if (vans) {
+		return (
+			<section className="mx-auto mt-13.5 mb-21 max-w-7xl px-6">
+				<h1 className="mb-6 font-bold text-3xl">Explore our van options</h1>
+				<div className="mb-10 flex flex-wrap items-center justify-between gap-4">
+					<VanTypeFilter
+						types={uniqueTypes}
+						selectedType={typeFilter}
+						onSelect={handleSelectFilter}
+					/>
+
+					{typeFilter && (
+						<button
+							type="button"
+							onClick={handleClearFilter}
+							className="cursor-pointer font-medium text-slate hover:underline"
+						>
+							Clear filters
+						</button>
+					)}
+				</div>
+				<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+					{displayedVans.map((van) => (
+						<VanCard key={van.id} van={van} typeFilter={typeFilter} />
+					))}
+				</div>
+			</section>
+		);
+	}
 }
